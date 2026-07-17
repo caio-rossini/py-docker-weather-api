a20ff954d7b9dde32345ed76c3faf1c0559e0252
Caio Rossini <caio.rossini.dev@gmail.com>
2026-07-17 19:03:34 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,39 @@
+import os
+import requests
+
+
+URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not API_KEY:
+        print("Error: API_KEY environment variable is missing.")
+        return
+
+    print(f"Performing request to Weather API for city {CITY}...")
+
+    params = {
+        "key": API_KEY,
+        "q": CITY
+    }
+
+    try:
+        response = requests.get(URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        location = data["location"]["name"]
+        country = data["location"]["country"]
+        local_time = data["location"]["localtime"]
+        temp_c = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+
+        print(f"{location}/{country} {local_time} Weather: {temp_c} Celsius, {condition}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
 
 
 if __name__ == "__main__":
